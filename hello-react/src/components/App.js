fd39b2c5e025412c2c9d1721590e10e9f968b4e3
SeoYeon Hwang <syhwang125@gmail.com>
2018-12-30 22:55:11 +0900

    hello-react

---
+++ hello-react/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import Counter from './Counter';
+
+class App extends Component {
+
+    render() {
+        return(
+            <Counter/>
+        );
+    }
+}
+
+export default App;
