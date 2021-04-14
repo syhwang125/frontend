b8b0b43bc2c13e6ea7969caf746d644461649c8f
syhwang125 <syhwang125@poscoict.com>
2021-04-14 21:13:51 +0900

    todo

---
+++ react-todo-mobx/BookList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {Container, List, ListItem} from '@material-ui/core';
+import BookListItem from './BookListItem';
+
+class BookList extends Component {
+    render() {
+        // const books = this.props.books;
+        const { books } = this.props; 
+        
+        const bookItems = books.map( book => {
+            return (
+                <ListItem key={book.ISBN}>
+                    <BookListItem book={book} />
+                </ListItem>
+            )
+        })
+
+        return (
+            <Container maxWidth='sm'>
+            <List>
+                {bookItems}
+
+                <ListItem>
+                    <BookListItem book={books[0]} />
+                    {books[0].title}
+                </ListItem>
+                <ListItem>
+                     {books[1].title}
+                </ListItem>
+                <ListItem>
+                    {books[2].title}
+                </ListItem>
+            </List> 
+            </Container>
+        )
+    }
+}
+
+export default BookList;
---
+++ react-todo-mobx/UserList.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Button, List, ListItemText} from '@material-ui/core';
+import axios from 'axios';     // server - http communication
+
+class UserList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users : [{
+                id : '1',
+                name : 'kang',
+            }, {
+                id : '2',
+                name : 'park'
+            }],
+            title : '', 
+        }
+    }
+
+    loadUsers() {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+             .then( response => {
+                 this.setState({
+                     users : response.data
+                 });
+             });
+    }
+
+    // props로부터 받은 데이터를 가지고 state를 변경하
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if(nextProps.title !== prevState.title) {
+            return { title : nextProps.title}
+        }
+        return null; 
+    }
+    
+    componentDidMount() {           // 컴포넌트가 마운트된 후 바로 호출됨
+        this.loadUsers();
+    }
+
+    render() {
+        const userList = this.state.users.map ( user => {
+            return <ListItemText primary={user.name} key={user.id} />
+        })
+
+
+        return (
+            <div>
+                <Button 
+                    onClick = { this.loadUsers.bind(this) } 
+                    variant='contained' color='primary'> Load </Button> 
+                <List>
+                    { userList }
+                </List>
+            </div>
+        )
+    }
+}
+
+export default UserList; 
