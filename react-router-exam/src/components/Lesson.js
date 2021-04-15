9466af421f1fa45f13a82d3c250cb7d1b53ad920
syhwang125 <syhwang125@poscoict.com>
2021-04-15 22:39:31 +0900

    react-route

--- react-router-exam/src/components/Lesson.js
+++ react-router-exam/src/components/Lesson.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import lessonsData from '../static-data/lessonsData'; 
 
 class Lesson extends Component {
-  render(){
-    return <div>Lesson ...</div>
-  }
-}
 
-export default Lesson;
+  render(){
+
+    const lessonId = this.props.match.params.lessonId;
+
+    console.log("@@@ " + lessonId);
+
+    const selectedLesson = lessonsData.find( lesson => {
+      return lesson.id === Number(lessonId);
+    })
+    
+    return <h2>{selectedLesson.description}</h2>
+    // return <h2>hello</h2>
+  }
+} 
+
+export default Lesson; 
--- react-router-exam/src/components/Lessons.js
+++ react-router-exam/src/components/Lessons.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+import { Grid, List, ListItem } from '@material-ui/core';
+import lessonsData from '../static-data/lessonsData';
+import { NavLink, Route } from 'react-router-dom';
+import Lesson from './Lessons.js';
+
 class Lessons extends Component {
 
   render(){
+
+    const links = lessonsData.map( lesson => { 
+      return (
+        <ListItem component='nav' key={lesson.id} >
+          <NavLink to={'/lessons/' + lesson.id}>{lesson.name}</NavLink>
+        </ListItem>
+      )
+    })
+
     return (
-      <div>Lessons List</div>
+      <Grid container spacing={2}>
+        <Grid item>
+          <List>{links}</List>
+        </Grid>
+        <Grid item>
+          <Route path='/lessons/:lessonId' component={Lesson} /> 
+        </Grid>
+      </Grid>
     )
   }
 }
