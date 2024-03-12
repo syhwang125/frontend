65ad1df8cb9f1f75c435cca265370c74795c1294
syhwang125 <syhwang125@poscoict.com>
2024-03-12 22:26:21 +0900

    vue-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!--
+      <app-header v-bind:프롭스 속성이름=상위컴포넌트의 데이터이름></app-header>
+    -->
+    <app-header v-bind:propsdata="str"
+                v-on:renew="renewStr">
+    </app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  }, 
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    'renewStr': function() {
+      this.str = 'hi';
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
